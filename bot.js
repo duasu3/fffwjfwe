6ea425d9f65b7671c5c5adcf2f65c2b668714ec5
duasu3 <50001033+duasu3@users.noreply.github.com>
2019-04-26 04:58:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,92 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '+'
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("Nothing To Say..","https://www.twitch.tv/idk");
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-var PrEfix = "f";
 
-client.on('message', message => {
-
-  if (!message.content.startsWith(PrEfix)) return;
-
-  var args = message.content.split(' ').slice(1);
-
-  var argresult = args.join(' ');
-
-  if (message.author.id !== '555361958036242448') return;
-
-if (message.content.startsWith(PrEfix + 'wt')) {
-
-client.user.setActivity(argresult, {type:'WATCHING'});
-
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-
-} else 
-
-if (message.content.startsWith(PrEfix + 'ls')) {
-
-client.user.setActivity(argresult, {type:'LISTENING'});
-
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-
-} else 
-
-if (message.content.startsWith(PrEfix + 'st')) {
-
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-
-} else
-  
-  
-  if (message.content.startsWith(PrEfix + 'pl')) {
-
-  client.user.setGame(argresult);
-
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-
-}
-
+client.on('message', function(message) {
+	const myID = "555361958036242448";
+    let args = message.content.split(" ").slice(1).join(" ");
+    if(message.content.startsWith(prefix + "setname")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الاسم الذي تريده.');
+        client.user.setUsername(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "stream")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args , 'https://twitch.tv/LOP');
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "offstatus")) {
+                if(message.author.id !== myID) return;
+client.user.setGame(args , '');
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "play")) {
+				        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "listen")) {
+				        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setActivity(args, {type:'LISTENING'});
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "watch")) {
+				        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setActivity(args, {type:'WATCHING'});
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "setavatar")) {
+				        if(message.author.id !== myID) return;
+        client.user.setAvatar(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+                if(!args) return message.reply('اكتب رابط الصوره التي تريدها.');
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    }
 });
+
+
+client.login(process.env.BOT_TOKEN);
